af392750ac138df23522034a7882f073a130ac19
paddymills <paddymills@pm.me>
2021-09-08 15:36:48 -0400

    basic layout and cards

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,74 @@
 <script>
-	export let name;
+	import { Github, Microsoftoutlook, Spotify, Youtube } from "@icons-pack/svelte-simple-icons";
+	import Monday from "./components/Monday.svelte";
+	import Card from "./components/Card.svelte";
+
+
+	let sites = [
+		{
+			title: "Hss",
+			links: [
+				{ title: "UltiPro",							url: "https://e14.ultipro.com/Login.aspx" },
+				{ title: "ExProc", 							url: "https://apps.high.net/ExProc/" },
+				{ title: "Markdown Notes", 					url: "https://dillinger.io/" },
+			]
+		},
+		{
+			title: "SigmaNest/NX",
+			links: [
+				{ title: "Sim Trans", 						url: "http://hssappserv1:3000/" },
+				{ title: "SigmaNest License Server",		url: "http://hssappserv1:5055/" },
+				{ title: "SigmaNest connect", 				url: "https://connect.sigmanest.com/" },
+				{ title: "Sim Trans Docs", 					url: "http://hssappserv1:3000/HelpCenter/content/home.htm" },
+				{ title: "SigmaNest Docs", 					url: "file:///C:/Program%20Files/SigmaTEK/SigmaNEST%20X1.7%20SP5/Help/HelpCenter/Content/Home.htm" },
+				{ title: "NX12 Docs", 						url: "https://docs.plm.automation.siemens.com/tdoc/nx/12/nx_help/#uid:index" },
+			]
+		},
+		{
+			title: "Dev",
+			links: [
+				{ title: "Riddle <rust fiddle>", 			url: "https://play.rust-lang.org/" },
+				{ title: "Rust::std", 						url: "https://doc.rust-lang.org/std/index.html" },
+				{ title: "crates.io", 						url: "https://crates.io/" },
+				{ title: "Rust <learn>", 					url: "https://www.rust-lang.org/learn" },
+				{ title: "Rust Nursery - Cookbook", 		url: "https://rust-lang-nursery.github.io/rust-cookbook/" },
+				{ title: "Rust-by-example", 				url: "https://doc.rust-lang.org/rust-by-example/" },
+				{ title: "Rust-by-example Ext", 			url: "https://rust-by-example-ext.com/" },
+				{ title: "Rust too-many-lists", 			url: "https://rust-unofficial.github.io/too-many-lists/" },
+				{ title: "MDN { JavaScript }", 				url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+			]
+		},
+	];
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div id="icons">
+		<a href="https://outlook.office.com"><Microsoftoutlook color="#0072C6" /></a>
+		<a href="https://github.com/paddymills"><Github color="#333" /></a>
+		<a href="https://open.spotify.com"><Spotify color="#1DB954" /></a>
+		<a href="https://youtube.com"><Youtube color="#FF0000" /></a>
+		<a href="https://production-control-force.monday.com/"><Monday /></a>
+	</div>
+
+	<div id="links">
+	{#each sites as group}
+		<Card {...group} />
+	{/each}
+	</div>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+<style >
+	div {
+		display: flex;
+		justify-content: space-around;
+		align-items: stretch;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	#icons {
+		justify-content: center;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	
+	#icons > a {
+		margin: 1em;
 	}
 </style>
---
+++ src/components/Card.svelte
@@ -0,0 +1,33 @@
+
+<script>
+    export let title;
+    export let links = [];
+
+</script>
+
+<card>
+    <h4>{title}</h4>
+
+    {#each links as item}
+        <a href={item.url}>{item.title}</a>
+    {/each}
+</card>
+
+<style>
+    card {
+        border: 1px solid white;
+        margin: 1em;
+        padding: 2em;
+        border-radius: 0.2em;
+        
+        display: flex;
+        flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+    }
+
+    h4 {
+        color: white;
+        text-decoration: underline;
+    }
+</style>
