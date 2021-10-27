af6d0b4403718d6cbe934bd728d92a9b9bde5c02
paddymills <paddymills@pm.me>
2021-10-27 07:38:03 -0400

    add kronos

--- src/App.svelte
+++ src/App.svelte
@@ -11,7 +11,8 @@
 				{ title: "UltiPro",							url: "https://e14.ultipro.com/Login.aspx" },
 				{ title: "ExProc", 							url: "https://apps.high.net/ExProc/" },
 				{ title: "Markdown Notes", 					url: "https://dillinger.io/" },
-				{ title: "SSRS",							url: "https://hiiboserv2.high.net/Reports/favorites" }
+				{ title: "SSRS",							url: "https://hiiboserv2.high.net/Reports/favorites" },
+				{ title: "Kronos",							url: "high.kronos.net/wfc/logon" }
 			]
 		},
 		{
