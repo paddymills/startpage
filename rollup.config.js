f5b5bf0b141e327fe25b6e9c2a6a73e347157e94
paddymills <paddymills@pm.me>
2022-09-14 11:43:37 -0400

    add json to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 
+import json from '@rollup/plugin-json';
+
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
@@ -68,7 +70,9 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
+
+		json()
 	],
 	watch: {
 		clearScreen: false
