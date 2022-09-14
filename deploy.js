b51e00c87e1ce52a0d539e7b445c4fc58791d224
paddymills <paddymills@pm.me>
2022-09-14 13:06:23 -0400

    simplify empty dest

--- deploy.js
+++ deploy.js
@@ -3,26 +3,17 @@ const fs = require('fs/promises');
 const path = require('path');
 const process = require('process');
 
-const src = './public/';
+const src = './public';
 const dest = path.join(process.env['USERPROFILE'], 'Documents', 'startpage');
 
-const recursive = { recursive: true };
-const emptyDir = async () => {
-	try {
-		const files = await fs.readdir(dest);
-		for (const file of files) {
-			await fs.rm( path.resolve(dest, file), recursive );
-		}
-	} catch (err) {
-		console.log("error emptying dest", err);
-	}
-}
 const deploy = async () => {
-	fs.cp(src, dest, recursive);
+	const recursive = { recursive: true };
+	
+	// empty dest folder
+	await fs.rm(dest, recursive);
+
+	// copy files
+	await fs.cp(src, dest, recursive);
 }
 
-// empty dest folder
-emptyDir();
-
-// copy files
 deploy();
