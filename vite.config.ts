e864911cb4c5e4a0a651d1fa72f040733d8be154
Patrick Miller <paddymills@proton.me>
2024-07-18 20:47:04 -0400

    add devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-// import devtools from 'solid-devtools/vite';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
 
 export default defineConfig({
   plugins: [
@@ -8,13 +8,21 @@ export default defineConfig({
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
-    // devtools(),
+    devtools({
+      autoname: true,
+      locator: {
+        targetIDE: "vscode",
+        key: "Ctrl",
+        jsxLocation: true,
+        componentLocation: true,
+      },
+    }),
     solidPlugin(),
   ],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
