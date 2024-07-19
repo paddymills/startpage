4ce633dbac6df732f372d2cab81754d23e0d712c
Patrick Miller <paddymills@proton.me>
2024-07-18 21:16:44 -0400

    minor additons from work

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { render } from "solid-js/web";
 
 import "./index.css";
 import { Startpage } from "./Startpage";
-import { Route, Router } from "@solidjs/router";
+import { Navigate, Route, Router } from "@solidjs/router";
 
 const root = document.getElementById("root");
 
@@ -13,9 +13,31 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   );
 }
 
+const Layout = (props: any) => {
+  return (
+    <>
+      <nav class="bg-gray-800 p-4 flex items-center space-x-2 justify-between">
+        <p>todo: theme</p>
+      </nav>
+      <main class="h-4/5 m-4 flex flex-col grow justify-around place-items-center">
+        {props.children}
+      </main>
+      <footer class="bg-gray-800 px-2 py-1 flex shrink-0 justify-between font-semibold tracking-wide select-none">
+        <p class="text-white">
+          &copy; {new Date().getFullYear()} by <span>someone</span>
+        </p>
+        <p class="text-white/50 text-xs place-self-center">
+          made with <span class="text-red-500">&hearts;</span> by Patrick Miller
+        </p>
+      </footer>
+    </>
+  );
+};
+
 render(
   () => (
-    <Router>
+    <Router root={Layout}>
+      <Route path="/" component={() => <Navigate href="/home" />} />
       <Route path="/:page" component={Startpage} />
     </Router>
   ),
