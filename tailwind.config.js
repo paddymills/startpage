4727b010552b15c2a8b1fd89074d070366a2a652
Patrick Miller <paddymills@proton.me>
2024-07-18 21:18:31 -0400

    fix tw compilation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./index.html", "./src/**/*.{ts, tsx}"];
+export const content = ["./index.html", "./src/**/*.{ts,tsx}"];
 export const theme = {
+  container: {
+    center: true,
+  },
   extend: {},
 };
 export const plugins = [];
